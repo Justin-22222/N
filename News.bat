0240e24cc3d14c3dd941301643446583b2fcc14a
LzSyp1923 <107896685+Justin-22222@users.noreply.github.com>
2023-05-10 21:43:46 +0800

    Update News.bat

--- News.bat
+++ News.bat
@@ -1,23 +1,33 @@
 @echo off
 echo :------------------------:
-echo :�����s�D���� (2023/5/10):
+echo :本次新聞報導 (2023/5/10) :
 echo :------------------------:-----------------:
-echo : OneOS 3.00.652 ��s�@��F��A�a�A�ӬݬݡI: 
-echo : SKiawm91/����				:
-echo :					:
-echo : �ھڳ̷s���������A�o�Ӫ��������\�h	:
-echo : ���e�A��Skiawm91�s�@���n�J�e����{�N��:
-echo : �o�Өt�Ϊ�Patch��LzSyp1923��o�P�}�o�A��:
-echo : OneOS���ѭ��j���U�I			:
-echo :					:
-echo :					:
-echo : OneOS 3.00.652 �w������s���e���G	:
-echo : OOBE�ЫإΤ�ש󤣥Τ@�w�o�Эӱb��F�I:
-echo : ���s²��ƪ��n�J�e��			:
-echo : ����x��s�ϧΤ�			:
-echo : �}�l���s�{���I			:
-echo :					:
-echo : �Ӥw���Q�״_��"�S��"���G		:
-echo : Command�Ҧ�����r�©��v�T��D�D	:
-echo : SysMGR�t�θ�T�״_��ܰ��D		:
+echo : OneOS 3.00.652 更新一堆東西，帶你來看看！	: 
+echo : SKiawm91/報導					:
+echo :							:
+echo : 根據最新內部消息，這個版本完善許多		:
+echo : 內容，由Skiawm91製作的登入畫面更現代化	:
+echo : 這個系統的Patch由LzSyp1923研發與開發，為	:
+echo : OneOS提供重大幫助！				:
+echo :							:
+echo :							:
+echo : OneOS 3.00.652 已知的更新內容有：		:
+echo : OOBE創建用戶終於不用一定得創個帳戶了！	:
+echo : 全新簡潔化的登入畫面				:
+echo : 控制台更新圖形化					:
+echo : 開始按鈕現身！					:
+echo :							:
+echo : 而已知被修復的"特性"有：				:
+echo : Command模式的綠字黑底影響到主題		:
+echo : SysMGR系統資訊修復顯示問題			:
+echo.
+echo : OneOS Patch工具
+echo : LzSyp1923/報導
+echo : 
+echo : 偷偷跟你說
+echo : 我製造了一個Patch
+echo : Patch Core版本目前為:Pre 0.4
+echo : 屬於未推出版本
+echo : 或者說根本只有做好安裝程式XD
+echo : 敬請期待:D
 echo -------------------------------------------:
